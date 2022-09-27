1fcdcc7c2e24509a7eee9034ab629a13249c430e
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2022-09-27 17:29:31 +0200

    ejercicios

---
+++ prueba/prueba.js
@@ -0,0 +1,22 @@
+console.log('Hola a todos')
+//alert('aviso a navegantes')
+console.info('Info message')
+console.error('Info error')
+debugger
+console.log('antes debug')
+
+console.log(parseInt('2.5' + "<br>"));
+console.log(parseInt('123 43' + "<br>"));
+console.log(parseInt('a 98'));
+
+console.log(isNaN('2.5' + "<br>"));
+console.log(isNaN('a 98'));
+console.log(isNaN(true));
+
+
+
+
+
+
+
+
