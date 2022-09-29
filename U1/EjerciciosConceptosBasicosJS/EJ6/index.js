ce2edfae64d1d7f8bf4a9bc943f89f575ef228dd
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2022-09-29 17:15:14 +0200

    ejercicios

--- U1/EjerciciosConceptosBasicosJS/EJ6/index.js
+++ U1/EjerciciosConceptosBasicosJS/EJ6/index.js
@@ -1,4 +1,10 @@
 // Crear un input y un button, cuando le demos al botón, se añadirá el texto que
 // hemos puesto en el input en un div vacío. Recuerda se añadirá el texto, no se
 // machaca lo que haya en el div.
-
+function add(){
+    var texto = document.getElementById("id2").value;
+    
+    const parrafo = document.createElement("p");
+    document.getElementById("div1").appendChild(parrafo);
+    document.getElementById("div1").lastChild.innerHTML = texto;
+}
