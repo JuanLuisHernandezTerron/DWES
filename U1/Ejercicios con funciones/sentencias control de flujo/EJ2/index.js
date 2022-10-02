17c9af9cb2bcfa9c71f81d0341940f52cf0a81c5
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2022-10-02 19:53:49 +0200

    ejercicios

--- U1/Ejercicios con funciones/sentencias control de flujo/EJ2/index.js
+++ U1/Ejercicios con funciones/sentencias control de flujo/EJ2/index.js
@@ -1,6 +1,11 @@
 function add(){
     let texto = document.getElementById("insertar").value; //Me guarda el valor del input de texto que he introducido
 
-    const lista = document.createElement("li"); // creo un elemento li, para la lista desordenada
+    if(texto ==""){
+        
+    }else{
+        const lista = document.createElement("li"); // creo un elemento li, para la lista desordenada
     document.getElementById("listaHTML").appendChild(lista).innerHTML = texto; //llamo al id del ul del html y creo subhijos(li) y le añado el texto
+    }
+
 }
---
+++ U1/Ejercicios con funciones/sentencias control de flujo/EJ3/index.js
@@ -0,0 +1,2 @@
+//Crea tres radiobuttons, cuando uno de ellos se seleccione que muestre un 
+//mensaje diciendo que opción se seleccionó
